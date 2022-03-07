11468fe0d0f19f54a8496d9dc896284fe4e73809
Helen V. Holmes <helen.v.holmes@gmail.com>
2022-03-07 13:30:10 -0500

    Throw in some skeleton markup and remove CRA boilerplate

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <main>
+        <h1>
+          Person Name
+        </h1>
+        <p>DOB: DOB</p>
+        <p>Place of Birth: POB</p>
+        <p>Children: children</p>
+        <p>Spouse: spouse</p>
+        <p>Parents: parents</p>
+      </main>
     </div>
   );
 }
