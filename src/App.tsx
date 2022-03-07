d0196643383e18151ab44fe672b136f7fc1733e1
Helen V. Holmes <helen.v.holmes@gmail.com>
2022-03-07 13:35:56 -0500

    Loads JSON file

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,32 @@
-import React from 'react';
-import './App.css';
+import React, { useEffect } from "react";
+import "./App.css";
 
 function App() {
+  const getData = () => {
+    fetch("./people.json", {
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+    })
+      .then(function (response) {
+        console.log(response);
+        return response.json();
+      })
+
+      .then(function (myJson) {
+        console.log(myJson);
+      });
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
   return (
     <div className="App">
       <main>
-        <h1>
-          Person Name
-        </h1>
+        <h1>Person Name</h1>
         <p>DOB: DOB</p>
         <p>Place of Birth: POB</p>
         <p>Children: children</p>
